afa8b36a262c904148b74bc1994ada1c6b8bdcb1
karthik <vkarthik308@gmail.com>
2021-11-11 22:54:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import BmiCalculator from './components/BMI';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p className="heading"> welcome </p>
+      <div className='row justify-content-center'>
+        <div className='col-lg-6 col-lg-offset-3'>
+          <BmiCalculator />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+    
